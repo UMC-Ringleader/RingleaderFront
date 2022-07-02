8c1137fc02943b99bbae9740bc40f1491e76095a
137pjy <pjy137@konkuk.ac.kr>
2022-07-02 10:23:23 +0900

    searchFragment

--- app/src/main/java/com/example/ringleaderfront/MainPageFragment.kt
+++ app/src/main/java/com/example/ringleaderfront/MainPageFragment.kt
@@ -1,20 +1,23 @@
 package com.example.ringleaderfront
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.ringleaderfront.databinding.FragmentMainPageBinding
+import kotlinx.android.synthetic.main.fragment_main_page.view.*
 import kotlinx.android.synthetic.main.review_card.view.*
 
 
 class MainPageFragment : Fragment() {
-
+    lateinit var binding:FragmentMainPageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       val binding=FragmentMainPageBinding.inflate(inflater,container,false)
+        binding=FragmentMainPageBinding.inflate(inflater,container,false)
         val reviewView1=getLayoutInflater().inflate(R.layout.review_card,null)
 
 
@@ -38,5 +41,25 @@ class MainPageFragment : Fragment() {
         binding.mainPageLl.addView(reviewView3)
 
         return binding.root
+
+
     }
+
+    override fun onStart() {
+
+            //'글 작성'버튼 눌렀을 때 writeFragment으로 화면이 넘어가야 하는데 안넘어감..
+
+//        //글 작성 버튼 화면 전환
+        if(binding.mainPageLl.extended_fab!=null) {
+            binding.mainPageLl.extended_fab.setOnClickListener {
+                val fragment = WriteFragment()
+                val transaction = fragmentManager?.beginTransaction()
+                transaction?.replace(R.id.main_page_fl, fragment)?.commit()
+            }
+        }
+
+
+        super.onStart()
+    }
+
 }
--- app/src/main/java/com/example/ringleaderfront/SearchFragment.kt
+++ app/src/main/java/com/example/ringleaderfront/SearchFragment.kt
@@ -1,9 +1,12 @@
 package com.example.ringleaderfront
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SearchView.*
+import androidx.fragment.app.Fragment
 import com.example.ringleaderfront.databinding.FragmentSearchBinding
 
 
@@ -14,6 +17,34 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding= FragmentSearchBinding.inflate(inflater,container,false)
+
+        var tags=arrayOf("스페인","와인","빠에야","정원","케이크")
+//        val tagsAdapter = ArrayAdapter(
+//            requireActivity()!!,
+//            R.layout.simple_list_item_1,
+//            tags
+//        )
+//        binding.searchLl.adapter=tagsAdapter
+
+        binding.searchSv.setOnQueryTextListener(object: OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchSv.clearFocus()
+                if(tags.contains(query)){
+                    //tag를 찾아서 tag가 있으면 해당 뷰를 addView로 보여줌
+                    //tagsAdapter.filter.filter(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //tagsAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+
         return binding.root
     }
 }
