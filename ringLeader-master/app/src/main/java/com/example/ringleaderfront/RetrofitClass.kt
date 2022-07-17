27919c0ff32686911c28f5df5d9a544cc3490c3e
roo-kie <yezi825@naver.com>
2022-07-17 17:08:14 +0900

    Update RetrofitClass.kt

--- ringLeader-master/app/src/main/java/com/example/ringleaderfront/RetrofitClass.kt
+++ ringLeader-master/app/src/main/java/com/example/ringleaderfront/RetrofitClass.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClass {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+//    private val retrofit = Retrofit.Builder()
+//        .baseUrl(BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
 }
