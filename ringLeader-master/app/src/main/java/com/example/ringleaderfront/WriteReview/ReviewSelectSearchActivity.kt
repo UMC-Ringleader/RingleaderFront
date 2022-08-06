f9a245042e9d5b7cd0bc37cf0fcc2ac3093e74c1
137pjy <pjy137@konkuk.ac.kr>
2022-08-07 00:14:59 +0900

    FEAT:review activity modify 08/07

--- ringLeader-master/app/src/main/java/com/example/ringleaderfront/WriteReview/ReviewSelectSearchActivity.kt
+++ ringLeader-master/app/src/main/java/com/example/ringleaderfront/WriteReview/ReviewSelectSearchActivity.kt
@@ -4,21 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import com.example.ringleaderfront.R
+import com.example.ringleaderfront.Store
 import com.example.ringleaderfront.databinding.ActivityLoginBinding
 import com.example.ringleaderfront.databinding.ActivityReviewSelectSearchBinding
 import com.example.ringleaderfront.gallary.ReviewSelectGallaryActivity
 
 class ReviewSelectSearchActivity : AppCompatActivity() {
     lateinit var binding: ActivityReviewSelectSearchBinding
-    lateinit var listAdapter: ArrayAdapter<String>
+    lateinit var listAdapter: StoreListViewAdapter
 
     // creating array list for listview
-    lateinit var storeList: ArrayList<String>;
+    lateinit var storeList: ArrayList<Store>
 
     // creating variable for searchview
     lateinit var searchView: SearchView
@@ -29,45 +32,51 @@ class ReviewSelectSearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        setUpData()
+        setUpList()
+        //setUpOnClickListener()
 
-        // initializing list and adding data to list
-        var storeList = ArrayList<String>()
-        storeList.add("음식점1")
-        storeList.add("음식점2")
-        storeList.add("음식점3")
-        storeList.add("음식점4")
+
+
+
 
         // initializing list adapter and setting layout
         // for each list view item and adding array list to it.
-        listAdapter = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_1,
-            storeList
-        )
+//        listAdapter = ArrayAdapter<String>(
+//            this,
+//            android.R.layout.simple_list_item_1,
+//            storeList
+//        )
 
-        // on below line setting list
-        // adapter to our list view.
-        binding.reviewSelectSearchLv.adapter = listAdapter
+
 
         // on below line we are adding on query
         // listener for our search view.
         searchView=binding.reviewSelectSearchSv
+        var storeListString=ArrayList<String>()
+        for(i in storeList){
+            storeListString.add(i.storeName)
+        }
         if (::searchView.isInitialized) {
+
             searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     Log.d("onQueryTextSubmit","눌려짐")
                     // on below line we are checking
                     // if query exist or not.
-                    if (storeList.contains(query)) {
+
+                    if (storeListString.contains(query)) {
                         // if query exist within list we
                         // are filtering our list adapter.
                         listAdapter.filter.filter(query)
                         if(query!=null) {
                             Log.d("query?","query is not null")
+
                             val intent = Intent(
                                 this@ReviewSelectSearchActivity,
-                                ReviewSelectGallaryActivity::class.java
+                                ReviewWriteActivity::class.java
                             )
+
                             startActivity(intent)
                         }
 
@@ -91,6 +100,39 @@ class ReviewSelectSearchActivity : AppCompatActivity() {
             })
 
         }
+
+    }
+
+    private fun setUpOnClickListener() {
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()){
+//
+//            public void onItemClick(AdapterView<?> adapterView, View view,int position,long l){
+//                Animal selectAnimal=(Animal)listView.getItemAtPosition(position);
+//                Intent showDetail=new Intent(getApplicationContext(),DetailActivity.class);
+//                showDetail.putExtra("id",selectAnimal.getId())
+//            }
+//        }
+
+    }
+
+    private fun setUpList() {
+        // on below line setting list
+        // adapter to our list view.
+        listAdapter=StoreListViewAdapter(storeList)
+        binding.reviewSelectSearchLv.adapter = listAdapter
+    }
+
+    private fun setUpData() {
+        // initializing list and adding data to list
+        storeList = ArrayList<Store>()
+        var store1=Store("음식점1",null)
+        var store2=Store("음식점2",null)
+        var store3=Store("음식점3",null)
+        var store4=Store("음식점4",null)
+        storeList.add(store1)
+        storeList.add(store2)
+        storeList.add(store3)
+        storeList.add(store4)
 
     }
 
--- ringLeader-master/app/src/main/java/com/example/ringleaderfront/WriteReview/ReviewWriteActivity.kt
+++ ringLeader-master/app/src/main/java/com/example/ringleaderfront/WriteReview/ReviewWriteActivity.kt
@@ -1,21 +1,36 @@
 package com.example.ringleaderfront.WriteReview
 
+import android.graphics.Color
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.esafirm.imagepicker.features.*
+import com.example.ringleaderfront.R
 import com.example.ringleaderfront.databinding.ActivityReviewWriteBinding
 
 
 class ReviewWriteActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityReviewWriteBinding
+    private lateinit var launcher: ImagePickerLauncher
+    private lateinit var launcher2: ImagePickerLauncher
+    private lateinit var selectedImages:ArrayList<Uri>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        selectedImages=ArrayList<Uri>()
 
         var reviewText: String = ""
         binding.writeBtn.isEnabled = false
@@ -42,16 +57,118 @@ class ReviewWriteActivity : AppCompatActivity() {
             Toast.makeText(this, reviewText, Toast.LENGTH_SHORT).show()
         }
 
-        imageLoad()
+
+
+
+        //+버튼 누르면 imagePicker불러와서 사진 선택
+        imagePickerLoad1()
 
     }
 
-    private fun imageLoad() {
 
-        var Uris =  intent.getSerializableExtra("UriArray") as ArrayList<Uri>
-        binding.reviewWriteLlIv1.setImageURI(Uris.get(0))
-        binding.reviewWriteLlIv2.setImageURI(Uris.get(1))
-        binding.reviewWriteLlIv3.setImageURI(Uris.get(2))
+    private fun imagePickerLoad1() {
+
+        //imagePicker 생성해서 이미지 url 매개변수로 넘기기
+        launcher = registerImagePicker { result ->
+            if (result.isNotEmpty()) {
+                for(i in result){
+                    selectedImages.add(i.uri)
+                }
+                setProfileImage(selectedImages)
+                Log.d("selectedImages",selectedImages.size.toString())
+
+            }
+
+        }
+
+        if(binding.reviewWriteLlIb.visibility==View.VISIBLE) {
+            binding.reviewWriteLlIb.setOnClickListener {
+                binding.reviewWriteLlIb.visibility = View.GONE
+                val config = ImagePickerConfig {
+                    mode = ImagePickerMode.MULTIPLE // 1장만 선택
+                    isFolderMode = false
+                    isIncludeVideo = false
+                    arrowColor = Color.WHITE
+                    imageTitle = "이미지를 선택하세요"
+                    doneButtonText = "추가" // returnMode가 NONE인 경우 표시됨
+                    isShowCamera = true
+                    returnMode = ReturnMode.NONE
+
+                }
+                launcher.launch(config)
+
+            }
+        }
+
+    }
+
+    private fun plusBtnLoad(width:Int,height:Int,imagesLayout:LinearLayout) {
+        val plusBtn = ImageButton(this@ReviewWriteActivity)
+        val ButtonLayoutParams = LinearLayout.LayoutParams(
+            width,
+            height,
+        )
+        plusBtn.layoutParams = ButtonLayoutParams
+        plusBtn.setImageResource(R.drawable.ic_baseline_add_24)
+        plusBtn.setBackgroundColor(Color.TRANSPARENT)
+        imagesLayout.addView(plusBtn)
+
+        //imagepicker
+
+//        launcher2 = registerImagePicker { result ->
+//            if (result.isNotEmpty()) {
+//                for(i in result){
+//                    selectedImages.add(i.uri)
+//                }
+//                setProfileImage(selectedImages)
+//                Log.d("selectedImages",selectedImages.size.toString())
+//            }
+//
+//        }
+//        plusBtn.setOnClickListener {
+//            val config = ImagePickerConfig {
+//                mode = ImagePickerMode.MULTIPLE // 1장만 선택
+//                isFolderMode = false
+//                isIncludeVideo = false
+//                arrowColor = Color.WHITE
+//                imageTitle = "이미지를 선택하세요"
+//                doneButtonText = "추가" // returnMode가 NONE인 경우 표시됨
+//                isShowCamera = true
+//                returnMode = ReturnMode.NONE
+//
+//            }
+//            launcher.launch(config)
+//        }
+
+
+
+    }
+
+
+    private fun setProfileImage(imageUris: ArrayList<Uri>) {
+        val imagesLayout: LinearLayout = findViewById(R.id.review_write_ll)
+        imagesLayout.removeAllViews()
+        val width = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            140f,
+            resources.displayMetrics
+        ).toInt()
+        val height = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            145f,
+            resources.displayMetrics
+        ).toInt()
+        for (i in imageUris) {
+            val selectedImage = ImageView(this@ReviewWriteActivity)
+            val imageLayoutParams = LinearLayout.LayoutParams(
+                width,
+                height,
+            )
+            selectedImage.layoutParams = imageLayoutParams
+            Glide.with(this).load(i).into(selectedImage)
+            imagesLayout.addView(selectedImage)
+        }
+        plusBtnLoad(width,height,imagesLayout)
 
     }
 }
